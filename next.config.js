c4168e340b988ead6d26aa87be2d1ab2ac5fbfdc
Maciej Reimann <maciej.reimann@gmail.com>
2023-03-26 18:36:58 +0200

    harvest.ts

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-module.exports = {
-  pageExtensions: ['page.tsx', 'page.js', 'endpoint.ts']
-};
-
+module.exports = async (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    pageExtensions: ['page.tsx', 'page.js', 'endpoint.ts'],
+  }
+  return nextConfig
+}
--- pages/api/real-estates/crawler/index.js
+++
@@ -1,9 +0,0 @@
-import { harvest } from "./harvest.js"
-
-try {
-    await harvest()
-    process.exit(0)
-} catch (error) {
-    console.error(error)
-    process.exit(1)
-}
